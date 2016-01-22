0796e7240bc306cdd6f3d352df4e996cf99e6405
Andreas Tack <andreas.tack@fmi.fi>
2016-01-22 14:28:40 +0200

    capitalize fortran syntax commands

--- netcdf_handle.f90
+++ netcdf_handle.f90
@@ -4,174 +4,174 @@
 !
 ! Provides file handle to netCDF file and procedures to manipulate
 
-module netcdf_handle
-  use netcdf
-  implicit none
+MODULE netcdf_handle
+  USE netcdf
+  IMPLICIT NONE
 
-  type nc_file
-    integer :: ncid
+  TYPE nc_file
+    INTEGER :: ncid
 
-  contains
-    procedure nc_create
-    procedure nc_defdim
-    procedure nc_defvar
-    procedure nc_enddef
-    procedure nc_open
-    procedure nc_close
-    procedure, private :: read1D => nc_read1D
-    procedure, private :: read2D => nc_read2D
-    generic, public :: nc_read => read1D, read2D
-    procedure, private :: write1D => nc_write1D
-    procedure, private :: write2D => nc_write2D
-    generic,public :: nc_write => write1D, write2D
-  end type
+  CONTAINS
+    PROCEDURE nc_create
+    PROCEDURE nc_defdim
+    PROCEDURE nc_defvar
+    PROCEDURE nc_enddef
+    PROCEDURE nc_open
+    PROCEDURE nc_close
+    PROCEDURE, PRIVATE :: read1D => nc_read1D
+    PROCEDURE, PRIVATE :: read2D => nc_read2D
+    GENERIC, PUBLIC :: nc_read => read1D, read2D
+    PROCEDURE, PRIVATE :: write1D => nc_write1D
+    PROCEDURE, PRIVATE :: write2D => nc_write2D
+    GENERIC,PUBLIC :: nc_write => write1D, write2D
+  END TYPE
 
-contains
+CONTAINS
   
-  integer function nc_create(this, file_name)
+  INTEGER FUNCTION nc_create(this, file_name)
     ! Create new netcdf-file and overwrite in case file already exists
     ! input value: file_name(char*) = name of the file
     ! return value: netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: file_name
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: file_name
 
     nc_create = nf90_create(file_name, NF90_CLOBBER, this%ncid)
-  end function nc_create
+  END FUNCTION nc_create
 
-  integer function nc_defdim(this, dim_name, dim_size)
+  INTEGER FUNCTION nc_defdim(this, dim_name, dim_size)
     ! Define new dimension
     ! input values: dim_name(char*) = name of the dimension
     !               dim_size(int) = size of the dimension 
     ! return value: netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: dim_name
-    integer :: dim_size, dim_id
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: dim_name
+    INTEGER :: dim_size, dim_id
 
     nc_defdim = nf90_def_dim(this%ncid,dim_name, dim_size, dim_id)
-  end function nc_defdim
+  END FUNCTION nc_defdim
 
-  integer function nc_defvar(this, var_name, NCTYPE, dim_ids)
+  INTEGER FUNCTION nc_defvar(this, var_name, NCTYPE, dim_ids)
     ! Define new variable
     ! input values: var_name(char*) = name of the variable
     !               NCTYPE = netcdf-type of the variable (e.g. NF90_INT)
     !               dim_ids (int(:)) = array containing dimension id's bound to newly defined variable
     ! return value: netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: var_name
-    integer :: NCTYPE, var_id
-    integer, dimension(:) :: dim_ids
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: var_name
+    INTEGER :: NCTYPE, var_id
+    INTEGER, DIMENSION(:) :: dim_ids
 
     nc_defvar = nf90_def_var(this%ncid, var_name, NCTYPE, dim_ids, var_id)
-  end function nc_defvar
+  END FUNCTION nc_defvar
 
-  integer function nc_enddef(this)
+  INTEGER FUNCTION nc_enddef(this)
     ! End definitions, leave define mode
     ! return value: netcdf error code
-    implicit none
-    class(nc_file) :: this
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
 
     nc_enddef = nf90_enddef(this%ncid)
-  end function nc_enddef
+  END FUNCTION nc_enddef
 
 
-  integer function nc_open(this,file_name)
+  INTEGER FUNCTION nc_open(this,file_name)
     ! Open existing netcdf-file in read-only mode
     ! input value: file_name (char*) = name of file to be opened
     ! return value: netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: file_name
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: file_name
 
     nc_open = nf90_open(file_name, NF90_NOWRITE, this%ncid)
-  end function nc_open
+  END FUNCTION nc_open
 
-  subroutine nc_read1D(this, var_name, data, stat, start, count, stride, map)
+  SUBROUTINE nc_read1D(this, var_name, data_out, nc_stat, start, count, stride, map)
     ! Read variable data from file
     ! input values: var_name (char*) = name of variable
-    !               data (real(:,:)) = the array containing data of the variable
-    !               stat (int) = netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: var_name
+    !               data_out (real(:)) = the array containing data of the variable
+    !               nc_stat (int) = netcdf error code
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: var_name
     !TODO Figure out some template like way to allow different type and dimension for data
-    real, dimension(:) :: data
-    integer, intent(out) :: stat
-    integer :: var_id
-    integer, optional, dimension(:) :: start, count, stride, map
+    REAL, DIMENSION(:) :: data_out
+    INTEGER, INTENT(out) :: nc_stat
+    INTEGER :: var_id
+    INTEGER, OPTIONAL, DIMENSION(:) :: start, count, stride, map
 
     ! Get the varid of the data variable, based on its name.
-    stat = nf90_inq_varid(this%ncid, var_name, var_id)
-    stat = nf90_get_var(this%ncid, var_id, data, start, count, stride, map)
-  end subroutine nc_read1D
+    nc_stat = nf90_inq_varid(this%ncid, var_name, var_id)
+    nc_stat = nf90_get_var(this%ncid, var_id, data_out, start, count, stride, map)
+  END SUBROUTINE nc_read1D
 
-  subroutine nc_read2D(this, var_name, data, stat, start, count, stride, map)
+  SUBROUTINE nc_read2D(this, var_name, data_out, nc_stat, start, count, stride, map)
     ! Read variable data from file
     ! input values: var_name (char*) = name of variable
     !               data (real(:,:)) = the array containing data of the variable
-    !               stat (int) = netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: var_name
+    !               nc_stat (int) = netcdf error code
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: var_name
     !TODO Figure out some template like way to allow generic type for data
-    real, dimension(:,:) :: data
-    integer, intent(out) :: stat
-    integer :: var_id
-    integer, optional, dimension(:) :: start, count, stride, map
+    REAL, DIMENSION(:,:) :: data_out
+    INTEGER, INTENT(out) :: nc_stat
+    INTEGER :: var_id
+    INTEGER, OPTIONAL, DIMENSION(:) :: start, count, stride, map
 
     ! Get the varid of the data variable, based on its name.
-    stat = nf90_inq_varid(this%ncid, var_name, var_id)
-    stat = nf90_get_var(this%ncid, var_id, data, start, count, stride, map)
-  end subroutine nc_read2D
+    nc_stat = nf90_inq_varid(this%ncid, var_name, var_id)
+    nc_stat = nf90_get_var(this%ncid, var_id, data_out, start, count, stride, map)
+  END SUBROUTINE nc_read2D
 
-  subroutine nc_write1D(this, var_name, data, stat, start, count, stride, map)
+  SUBROUTINE nc_write1D(this, var_name, data_in, nc_stat, start, count, stride, map)
+    ! Write variable data to file
+    ! input values: var_name (char*) = name of variable
+    !               data_in (real(:)) = the array containing data of the variable
+    !               nc_stat (int) = netcdf error code
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: var_name
+    !TODO Figure out some template like way to allow generic type for data
+    REAL, DIMENSION(:) :: data_in
+    INTEGER, INTENT(out) :: nc_stat
+    INTEGER :: var_id
+    INTEGER, OPTIONAL, DIMENSION(:) :: start, count, stride, map
+
+
+    ! Get the varid of the data variable, based on its name.
+    nc_stat = nf90_inq_varid(this%ncid, var_name, var_id)
+    nc_stat = nf90_put_var(this%ncid, var_id, data_in, start, count, stride, map)
+  END SUBROUTINE nc_write1D
+
+  SUBROUTINE nc_write2D(this, var_name, data_in, nc_stat, start, count, stride, map)
     ! Write variable data to file
     ! input values: var_name (char*) = name of variable
     !               data (real(:,:)) = the array containing data of the variable
-    !               stat (int) = netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: var_name
-    !TODO Figure out some template like way to allow generic type for data
-    real, dimension(:) :: data
-    integer, intent(out) :: stat
-    integer :: var_id
-    integer, optional, dimension(:) :: start, count, stride, map
-
-
-    ! Get the varid of the data variable, based on its name.
-    stat = nf90_inq_varid(this%ncid, var_name, var_id)
-    stat = nf90_put_var(this%ncid, var_id, data, start, count, stride, map)
-  end subroutine nc_write1D
-
-  subroutine nc_write2D(this, var_name, data, stat, start, count, stride, map)
-    ! Write variable data to file
-    ! input values: var_name (char*) = name of variable
-    !               data (real(:,:)) = the array containing data of the variable
-    !               stat (int) = netcdf error code
-    implicit none
-    class(nc_file) :: this
-    character (len = *) :: var_name
+    !               nc_stat (int) = netcdf error code
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
+    CHARACTER (len = *) :: var_name
 
     !TODO Figure out some template like way to allow generic type for data
-    real, dimension(:,:) :: data
-    integer, intent(out) :: stat
-    integer :: var_id
-    integer, optional, dimension(:) :: start, count, stride, map
+    REAL, DIMENSION(:,:) :: data_in
+    INTEGER, INTENT(out) :: nc_stat
+    INTEGER :: var_id
+    INTEGER, OPTIONAL, DIMENSION(:) :: start, count, stride, map
 
     ! Get the varid of the data variable, based on its name.
-    stat = nf90_inq_varid(this%ncid, var_name, var_id)
-    stat = nf90_put_var(this%ncid, var_id, data, start, count, stride, map)
-  end subroutine nc_write2D
+    nc_stat = nf90_inq_varid(this%ncid, var_name, var_id)
+    nc_stat = nf90_put_var(this%ncid, var_id, data, start, count, stride, map)
+  END SUBROUTINE nc_write2D
 
-  integer function nc_close(this)
+  INTEGER FUNCTION nc_close(this)
   ! Close the file, freeing all resources
-    implicit none
-    class(nc_file) :: this
+    IMPLICIT NONE
+    CLASS(nc_file) :: this
 
     nc_close = nf90_close(this%ncid)
-  end function nc_close
+  END FUNCTION nc_close
 
-end module netcdf_handle
+END MODULE netcdf_handle
